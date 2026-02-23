5cf72e388d8465c17865e0d3eee1a3779af8d21f
piotrekDo <piotr.domagalski@yahoo.com>
2026-02-23 14:02:19 +0100

    transactions virtalized list

--- src/components/transaction_page/TransactionsList.tsx
+++ src/components/transaction_page/TransactionsList.tsx
@@ -1,9 +1,9 @@
-import { useMemo } from 'react'
 import { useSubcategories } from '@/hooks/useSubcategories'
 import type { Transaction } from '@/model/Transaction'
-import { Text, VStack } from '@chakra-ui/react'
+import { Box, HStack, Text } from '@chakra-ui/react'
+import { useMemo, useRef } from 'react'
+import { GroupedVirtuoso } from 'react-virtuoso'
 import { TransactionCard } from '../transaction/card/TransactionCard'
-
 interface Props {
   filteredTransactions: Transaction[]
   isFetching: boolean
@@ -17,72 +17,97 @@ export const formatDayHeader = (date: Date) =>
     year: 'numeric',
   })
 
-export const TransactionsList = ({
-  filteredTransactions,
-  isFetching,
-}: Props) => {
+export const TransactionsList = ({ filteredTransactions, isFetching, }: Props) => {
+  const virtuoso = useRef<any>(null)
+
   const { data: subcategories } = useSubcategories()
 
-  const groupedTransactions = useMemo(() => {
-    // 1️⃣ sortujemy malejąco (najmłodsze pierwsze)
+  const { sorted, groups, groupCounts, firstItemIndexes } = useMemo(() => {
     const sorted = [...filteredTransactions].sort(
-      (a, b) =>
-        new Date(b.tranDate).getTime() -
-        new Date(a.tranDate).getTime()
+      (a, b) => new Date(b.tranDate).getTime() - new Date(a.tranDate).getTime()
     )
 
-    const map = new Map<
-      string,
-      {
-        date: Date
-        transactions: Transaction[]
-        sum: number
-      }
-    >()
+    type Group = { date: Date; count: number; sum: number }
 
+    const map = new Map<string, Group>()
     for (const t of sorted) {
-      const dateObj = new Date(t.tranDate)
-      const key = dateObj.toDateString()
-
-      if (!map.has(key)) {
-        map.set(key, {
-          date: dateObj,
-          transactions: [],
-          sum: 0,
-        })
-      }
-
-      const group = map.get(key)!
-      group.transactions.push(t)
-      group.sum += t.amount
+      const d = new Date(t.tranDate)
+      const key = d.toDateString()
+      if (!map.has(key)) map.set(key, { date: d, count: 0, sum: 0 })
+      const g = map.get(key)!
+      g.count += 1
+      g.sum += t.amount
     }
 
-    return Array.from(map.values())
+    const groups = Array.from(map.values())
+    const groupCounts = groups.map(g => g.count)
+
+    const firstItemIndexes = groupCounts.reduce(
+      ({ firsts, offset }, count) => ({
+        firsts: [...firsts, offset],
+        offset: offset + count,
+      }),
+      { firsts: [] as number[], offset: 0 }
+    ).firsts
+
+    return { sorted, groups, groupCounts, firstItemIndexes }
   }, [filteredTransactions])
 
   return (
-    <VStack align="stretch" gap={5}>
-      {groupedTransactions.map(group => (
-        <VStack key={group.date.toDateString()} align="stretch" gap={2}>
-          <Text
-            alignSelf="flex-start"
-            color="blackAlpha.800"
-            fontWeight="700"
-            fontSize="md"
-          >
-            {formatDayHeader(group.date)} — {group.sum.toFixed(2)} zł
-          </Text>
+    <HStack justify={'start'} h="70vh" align={'start'} position={'relative'}>
+      <Box w={'700px'} h="70vh" color={'blackAlpha.800'} 
+     >
+        <GroupedVirtuoso
+          ref={virtuoso}
+          style={{ height: '100%' }}
+          groupCounts={groupCounts}
+          groupContent={groupIndex => {
+            const group = groups[groupIndex]
+            if (!group) return <div style={{ height: 32 }} />
 
-          {group.transactions.map(t => (
-            <TransactionCard
-              key={t.systemId}
-              tran={t}
-              isFetching={isFetching}
-              subcategories={subcategories || []}
-            />
-          ))}
-        </VStack>
-      ))}
-    </VStack>
+            return (
+              <Box key={groupIndex} px={3} py={2} bg='#F5F1EE'>
+                <Text fontWeight="700">
+                  {formatDayHeader(group.date)} {group.count} transakcje — {group.sum.toFixed(2)} zł
+                </Text>
+              </Box>
+            )
+          }}
+
+          itemContent={(itemIndex, groupIndex) => {
+            const group = groups[groupIndex]
+            if (!group) return <div style={{ height: 80 }} />
+            const t = sorted[itemIndex]
+            if (!t) return <div style={{ height: 250, backgroundColor: '' }} >{itemIndex} g: {groupIndex}</div>
+
+            return (
+              <Box key={itemIndex + ' ' + groupIndex} px={2} py={2}>
+                <TransactionCard
+                  tran={t}
+                  isFetching={isFetching}
+                  subcategories={subcategories || []}
+                />
+              </Box>
+            )
+          }}
+        />
+      </Box>
+
+      <Box position={'absolute'} right={'-300px'} h="70vh" overflowY="auto"   >
+        {groups.map((group, index) => (
+          <Box key={index} py={1}>
+            <Text
+              cursor="pointer"
+              color="blue.500"
+              onClick={() => {
+                virtuoso.current?.scrollToIndex({ index: firstItemIndexes[index] })
+              }}
+            >
+              {formatDayHeader(group.date)}
+            </Text>
+          </Box>
+        ))}
+      </Box>
+    </HStack>
   )
 }
